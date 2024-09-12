fa40143215fd5599f6975a02eef184a6ca50efe7
Haletran <bpasquier123@gmail.com>
2024-09-12 19:50:25 +0200

    feat: add settings (modify db), render login whenever the user skip

---
+++ src/scripts/actions.js
@@ -0,0 +1,34 @@
+document.getElementById("start").addEventListener("click", function () {
+  Array.from(document.getElementsByClassName("game")).forEach(
+    function (element) {
+      element.style.display = "block";
+    },
+  );
+  document.getElementById("start").style.display = "none";
+  document.getElementById("settings").style.display = "none";
+});
+
+document.getElementById("settings").addEventListener("click", function () {
+  document.getElementById("start").style.display = "none";
+  document.getElementById("settings").style.display = "none";
+  Array.from(document.getElementsByClassName("settings_box")).forEach(
+    function (element) {
+      element.style.display = "block";
+    },
+  );
+});
+
+document.getElementById("save").addEventListener("click", function () {
+  //delete old cookie
+  localStorage.removeItem("db");
+  localStorage.setItem("db", document.getElementById("db").value);
+  // reload the page to get the new db
+  location.reload();
+  document.getElementById("start").style.display = "";
+  document.getElementById("settings").style.display = "";
+  Array.from(document.getElementsByClassName("settings_box")).forEach(
+    function (element) {
+      element.style.display = "none";
+    },
+  );
+});
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,23 +2,43 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-fetch("db.json")
+// Get wich db to fetch with the cookie
+
+let url;
+const db = localStorage.getItem("db");
+console.log(db);
+if (db == "PISCINE") {
+  url = "../db/pool.json";
+} else if (db == "ALL") {
+  url = "../db/all.json";
+} else if (db == "2023") {
+  url = "../db/2023.json";
+} else if (db == "2022") {
+  url = "../db/2022.json";
+} else {
+  url = "../db/all.json";
+}
+
+fetch(url)
   .then((response) => response.json())
   .then((data) => {
     // VARIABLES
     const img = document.getElementById("img");
     const box = document.getElementById("input");
     const pass = document.getElementById("pass");
+    const name = document.getElementById("name");
     const scoreValue = document.getElementById("scoreValue");
     const easteregg = document.getElementById("eastervalue");
     let scoreCounter = 0;
     let eastereggCounter = 0;
+    let easter_found = [];
     let already_found = [];
 
     // Get a random user from the db and pass it to the box
     function updateRandomUser() {
       const randomIndex = Math.floor(Math.random() * data.length);
       const randomImg = data[randomIndex].image_url_medium;
+      const oldLogin = data[randomIndex].login;
       const randomLogin = data[randomIndex].login;
 
       // encode the img url in base64
@@ -37,11 +57,17 @@ fetch("db.json")
     let currentLogin = updateRandomUser();
 
     // PASS the current login but lose a point
-    pass.addEventListener("click", () => {
+    pass.addEventListener("click", async () => {
       box.setAttribute("aria-invalid", "");
       box.value = "";
-      console.log(currentLogin); // need to render this in the frontend
+      img.style.filter = "blur(5px)";
+      name.style.display = "block";
+      name.textContent = currentLogin;
+      await sleep(350);
       currentLogin = updateRandomUser();
+      await sleep(50);
+      img.style.filter = "blur(0px)";
+      name.style.display = "none";
       if (scoreCounter > 0) {
         scoreCounter--;
         scoreValue.textContent = scoreCounter;
@@ -51,26 +77,26 @@ fetch("db.json")
     // EASTER EGG check and alert
     // CHANGE THEM TO YOUR LIKING
     function check_easteregg(login) {
-      if (login === "bapasqui" && !already_found.includes("bapasqui")) {
+      if (login === "bapasqui" && !easter_found.includes("bapasqui")) {
         eastereggCounter++;
         alert("Oui c'est moi !");
-        already_found.push("bapasqui");
+        easter_found.push("bapasqui");
       } else if (
         login == "motherlode" &&
-        !already_found.includes("motherlode")
+        !easter_found.includes("motherlode")
       ) {
         eastereggCounter++;
         scoreCounter = 9999999;
         alert("INFINITE SCORING !");
-        already_found.push("motherlode");
-      } else if (login == "42" && !already_found.includes("42")) {
+        easter_found.push("motherlode");
+      } else if (login == "42" && !easter_found.includes("42")) {
         eastereggCounter++;
         alert("42");
-        already_found.push("42");
+        easter_found.push("42");
       }
-      if (eastereggCounter == 3 && !already_found.includes("3")) {
+      if (eastereggCounter == 3 && !easter_found.includes("3")) {
         alert("You found all the easter eggs ! Congrats !");
-        already_found.push("3");
+        easter_found.push("3");
       }
       easteregg.textContent = eastereggCounter;
     }
@@ -86,6 +112,7 @@ fetch("db.json")
           await sleep(150);
           e.target.value = "";
           box.setAttribute("aria-invalid", "");
+          already_found.push(currentLogin);
           currentLogin = updateRandomUser();
         } else {
           box.setAttribute("aria-invalid", "true");
@@ -99,12 +126,3 @@ fetch("db.json")
   .catch((error) => {
     console.error("Error de db", error);
   });
-
-document.getElementById("start").addEventListener("click", function () {
-  Array.from(document.getElementsByClassName("game")).forEach(
-    function (element) {
-      element.style.display = "block";
-    },
-  );
-  document.getElementById("start").style.display = "none";
-});
