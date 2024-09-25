f81df5cb41b38e94bd2512b07e59e0be0e26c258
Baptiste-Pasquier <bpasquier123@gmail.com>
2024-09-25 22:48:08 +0200

    fix: not && but || in recursive

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -50,7 +50,7 @@ fetch(url)
       const randomIndex = Math.floor(Math.random() * data.length);
       const randomImg = data[randomIndex].image_url_medium;
       const randomLogin = data[randomIndex].login;
-      if (already_found.includes(randomLogin) && already_found.length < data.length) {
+      if (already_found.includes(randomLogin) || already_found.length < data.length) {
         return updateRandomUser();
       }
 
